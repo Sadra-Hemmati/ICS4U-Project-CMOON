e9421f483cf96a3a6e00fc9b6fccb7550e8d5afc
Sadra Hemmati <sadra.hemmati@gmail.com>
2025-11-06 08:21:25 +0000

    Replace the two letter sybmols (Su, Mo, Tu ect) with Single letters (S,

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,8 +1,10 @@
+
 "use client"
 
 import * as React from "react"
 import { ChevronLeft, ChevronRight } from "lucide-react"
 import { DayPicker } from "react-day-picker"
+import { format } from 'date-fns';
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
@@ -52,6 +54,9 @@ function Calendar({
           "aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
+      }}
+      formatters={{
+        formatWeekdayName: (day) => format(day, 'ccccc'),
       }}
       components={{
         IconLeft: ({ className, ...props }) => (
